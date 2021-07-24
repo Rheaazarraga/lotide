88dc329e9ce27f7269546d6d1d92b652f254daa4
Rhea <84409001+Rheaazarraga@users.noreply.github.com>
2021-07-24 13:06:44 -0700

    Update assertEqual.js
    changed to original simplified approach

--- assertEqual.js
+++ assertEqual.js
@@ -1,22 +1,3 @@
-const eqArrays = function(actual, expected) {
-
-  //If array lengths do not match, false.
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  // if array lengths are same, compare each element
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-
-  // if false was not returned, the arrays must be identical
-  return true;
-};
-
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} ✅ ${expected}`);
