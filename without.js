b8a81ef075ace23e152203678b7fdb2bf2f9e101
Rheaazarraga <rheaazarraga@gmail.com>
2021-07-29 00:00:41 +0000

    without() function takes in 2 arrays as args: 1st is source, 2nd tells us which item(s) to remove from 1st array. Added more test cases for fun

--- without.js
+++ without.js
@@ -42,7 +42,10 @@ console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["1", '2', "3"], [1, 2, "3"])); // => ["1", "2"]
 console.log(without([5, 4, 3], [5, 4, 3, 2]));
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// MORE TEST CASES
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", '2', "3"], [1, 2, "3"])); // => ["1", "2"]
+const words = ["negroni", "mimosa", "sidecar"];
+console.log(without(words, ["mimosa"])); //returns "mimosa" 
+console.log(assertArraysEqual(words, ["negroni", "mimosa", "sidecar"]));
+console.log(assertArraysEqual(without(words, ["mimosa"]), ["mimosa"]));
