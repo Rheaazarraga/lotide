dd9e3b525496c7c22e21acfb741ec0e4b5fa68ec
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-02 01:10:20 +0000

    Checking assertions passed or failed with firstName test code

--- countOnly.js
+++ countOnly.js
@@ -12,6 +12,31 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   //create an empty object that will store the items counted and the times counted
   const results = {};
+  for (const item of allItems) { //for loop looking for element of the allItems array
+  console.log(item);
+  
+  }
+  
      //loop through the itemsToCount object
     // The function should report back how many instances of each string were found in the allItems array of strings.
-}
+  return results;
+  }
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+  assertEqual(result1["Agouhanna"], undefined);
