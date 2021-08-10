1bf184e00bcccf3ea6628d2eda834eb897a60e01
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-10 00:25:53 +0000

    FIxed linting issues

--- head.js
+++ head.js
@@ -6,8 +6,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-function head(array) {
+const head = function(array) {
   return (array[0]);
-}
+};
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
