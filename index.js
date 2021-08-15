3e005797c5eb33217af85c439ec855923ee4f569
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 01:16:47 +0000

    Implemented object shorthand

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
+const map = require('./map');
+const flatten = require('./flatten');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+
+
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  map,
+  flatten,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  findKey,
+  letterPositions,
+  takeUntil,
+  without,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly
+
 };
