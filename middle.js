28e943d1d3ca09a9ab931b8e71571e087125cf0f
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 00:25:50 +0000

    Cleaned up code and comments

--- middle.js
+++ middle.js
@@ -1,24 +1,33 @@
 const eqArrays = require("./eqArrays");
 const assertArraysEqual = require("./assertArraysEqual");
 
-const middle = function(array) {
-  let result = [];
-  if (array.length % 2 === 1) {
-    console.log('odd -----');
-    const middleIndex = (array.length - 1) / 2;
-    const middleValue = array[middleIndex];
-    result.push(middleValue);
-  } else {
-    const secondMiddleIndex = (array.length / 2);
-    const firstMiddleIndex = secondMiddleIndex - 1;
-    const firstMiddleValue = array[firstMiddleIndex];
-    const secondMiddleValue = array [secondMiddleIndex];
-    result.push(firstMiddleValue);
-    result.push(secondMiddleValue);
-    console.log('even +++++');
-  }
-  console.log('> > > >', result);
-  return result;
-};
 
+const middle = function(array) {
+  const midArr = [];
+  if (array.length > 2 && array.length % 2 === 0) {
+    const midIndex = (array.length - 1) / 2;
+    midArr.push(array[Math.floor(midIndex)]);
+    midArr.push(array[Math.ceil(midIndex)]);
+    return (midArr);
+  }
+  if (array.length > 2 && array.length % 2 !== 0) {
+    const midIndex = (array.length - 1) / 2;
+    midArr.push(array[(midIndex)]);
+    return (midArr);
+  }
+  return midArr;
+};
 module.exports = middle;
+
+// middle finds the middle element in an array for all arrays larger than 2 elements and returns it in an array
+// if the array's length is even, return the 2 middle elements
+// if the array's length is odd, return the middle element
+// if the array has less than 2 elements, return an empty array
+// if the array is longer than 2 elements AND the array's length is even
+// find the middle index (Will be a decimal)
+// round down and set it as the first element in the midArr
+// round up set it as the second element in the midArr
+// if the array is longer than 2 elements AND the array's length is odd
+// find the middle index
+// set the midpointArray to the element as an array
+// return an empty array
