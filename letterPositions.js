d702a81af8bf99aeb92436d3ba71c10730b752f6
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 19:27:45 +0000

    Cleaned up code and test calls

--- letterPositions.js
+++ letterPositions.js
@@ -32,21 +32,5 @@ console.log(results2);
 assertArraysEqual(results2.m, [6, 7]);
 assertArraysEqual(results2.o, [2, 16, 17]);
 
-// TEST CALLS
-
-const sentence1 = "hello and goodbye";
-const results1 = letterPositions(sentence1);
-
-console.log(results1);
-assertArraysEqual(results1.e, [1, 16]);
-assertArraysEqual(results1.o, [4, 11, 12]);
-
-const sentence2 = "programming is cool";
-const results2 = letterPositions(sentence2);
-
-console.log(results2);
-assertArraysEqual(results2.m, [6, 7]);
-assertArraysEqual(results2.o, [2, 16, 17]);
-
 //EXPORT
 module.exports = letterPositions;
