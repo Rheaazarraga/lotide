a9dde516207891dd9912e32d13a22a0f88916355
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 19:33:44 +0000

    Refactored code, added module exports

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ✅ ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} ❌ ${expected}`);
-  }
-};
-
+const assertEqual = require("./assertEqual");
 
 //Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
 
@@ -16,6 +9,9 @@ const findKeyByValue = function(object, value) {
     }
   }
 };
+
+//EXPORT
+module.exports = findKeyByValue;
 
 //TEST CASES:
 
