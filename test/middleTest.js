ef34816ec7c462ace084b9081eb26a2fc88bc701
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 00:42:18 +0000

    Implemented mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,43 @@
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
-//TEST CASES:
 
-middle([3, 4, 5, 6, 7, 8]);
+describe("#middle", () => {
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it ("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+ 
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
+
+
+//TEST CASES: 
+
+//middle([3, 4, 5, 6, 7, 8]);
 
 // [3, 4, 5, 6, 7] //middleValue = 5, indexOfMiddle = 2, arrayLength = 5 || middleIndex = (arrayLength - 1) / 2 || (5 - 1) / 2
 
@@ -16,12 +49,11 @@ middle([3, 4, 5, 6, 7, 8]);
 
 // [3, 4, 5, 6, 7, 8] arrayLength = 6, middleValue = 5, 6, middleIndex = 2, 3
 
-// TEST CASES:
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-(assertArraysEqual([5, 4, 3, 2, 1], [5, 4, 2 ,1]));
-(assertArraysEqual(["bita " + "bean " + "baby"], ["bita " + "bean " + "baby"]));
+// // TEST CASES:
+// assertArraysEqual(middle([1])); // => []
+// assertArraysEqual(middle([1, 2])); // => []
+// assertArraysEqual(middle([1, 2, 3])); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5])); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4])); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+// assertArraysEqual([5, 4, 3, 2, 1], [5, 4, 2 ,1]));
