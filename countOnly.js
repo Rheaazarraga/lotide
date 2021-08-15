88f651cf9c4979d09eb665fff0c57f82a6d4f74c
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-15 18:48:27 +0000

    Refactored code and added module exports

--- countOnly.js
+++ countOnly.js
@@ -17,43 +17,5 @@ const countOnly = function(allItems, itemsToCount) {
   return results; // The function should report back how many instances of each string were found in the allItems array of strings.
 };
   
-//TEST CODE:
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-  
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-  
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-
-//MORE TEST CALLS:
-
-const teaTypes = [
-  "earl grey",
-  "matcha",
-  "green tea",
-  "chamomille",
-  "oolong",
-  "green tea",
-  "chamomille",
-  "chamomille",
-  "black tea"
-];
-
-const result2 = countOnly(teaTypes, { "matcha" : true, "chamomille" : true, "oolong" : false, "black tea" : true});
-
-assertEqual(result2["matcha"], 1);
-assertEqual(result2["chamomille"], 3);
-assertEqual(result2["green tea"], 2);
-assertEqual(result2["oolong"], undefined);
+//EXPORT
+module.exports = countOnly;
