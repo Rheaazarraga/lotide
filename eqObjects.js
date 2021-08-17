9ad18661890d7f23df20f08a8f7a8030624ff57c
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-17 22:47:24 +0000

    Removed old test cases, imported eqArrays and added module exports

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,3 @@
-const assertEqual = require("./assertEqual");
 const eqArrays = require("./eqArrays");
 
 const eqObjects = function(object1, object2) {
@@ -38,26 +37,3 @@ const eqObjects = function(object1, object2) {
 
 //EXPORT
 module.exports = eqObjects;
-
-//TEST CASES:
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
-
-// check lengths are the same
-// check if array of keys are the same
-//if arrays are not equal, return false
-//if they are equal, keep going / do nothing
-//loop through one of the objects arrays, and check if it exists in the other object
-// check if object contents are the same
