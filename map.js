2092202277316a34177081140527cb6448e7aa55
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-17 22:24:04 +0000

    Removed old test cases, cleaned up notes, and added module exports

--- map.js
+++ map.js
@@ -1,22 +1,5 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length)
-    return false;
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i])
-      return false;
-  }
-  return true;
-};
+//creates a new array populated with the results of a callback function on every element in the specified array.
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} ✅ ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} ❌ ${expected}`);
-  }
-};
-
-const words = ["Bita", "is", "the", "baby", "of", "this", "house"];
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -25,6 +8,4 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
-assertArraysEqual(map(words, word => word[0]), ['B', 'i', 't', 'b', 'o', 't', 'h']);
-console.log(results1);
+module.exports = map;
