1ba4991724ada4021153f16a81bd3c745b7db515
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-17 22:20:02 +0000

    Removed old test cases, cleaned up notes and added module exports

--- findKey.js
+++ findKey.js
@@ -1,12 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ✅ ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} ❌ ${expected}`);
-  }
-};
-
-/**Implement the function findKey which takes in an object and a 
+/**
+ * Implement the function findKey which takes in an object and a 
  * callback
  * for in loop to iterate through the keys in the object
  * return the first key
@@ -22,34 +15,4 @@ const findKey = function(object, callback) {
   }
 };
 
-//TEST CASES:
-
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma" will satisfy the callback becasue it's the first key with the score of 2
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
-
-//2ND TEST CASE:
-
-const bestVikings = {
-  "Ragnar Lothbrok" : { rank : "Legendary King" },
-  "Bjorn Ironside" : { rank : "King of Kings" },
-  "Lagertha the Shield Maiden" : { rank : "Queen of Vikings" },
-  "Ivar the Boneless" : { rank : "The Unpredictable"},
-  "Ubbe Lothbrok" : {rank : "The Western Explorer"}
-};
-
-assertEqual(findKey(bestVikings, x => x.rank === "Queen of Vikings"), "Lagertha the Shield Maiden");
+module.exports = findKey;
