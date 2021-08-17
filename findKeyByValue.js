05e3af896e7dc5e79fd9ad875e7e418a6c73a3d8
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-17 23:02:02 +0000

    Removed all test cases, cleaned up notes, and added module exports

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -3,7 +3,7 @@ const assertEqual = require("./assertEqual");
 //Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
 
 const findKeyByValue = function(object, value) {
-  for (const key in object) { //search through object keys with for in loop
+  for (const key in object) {
     if (object[key] === value) {
       return key;
     }
@@ -12,27 +12,3 @@ const findKeyByValue = function(object, value) {
 
 //EXPORT
 module.exports = findKeyByValue;
-
-//TEST CASES:
-
-const bestTVShowsByGenre = {
-  history: "Vikings",
-  comedy: "Rick & Morty",
-  drama:  "The Sopranos"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Sopranos"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Vikings"), "history");
-
-//MORE TEST CASES:
-
-const parksInVan = {
-  WestEnd : "Stanley Park",
-  Yaletown : "David Lam Park",
-  UBC : "Pacific Spirit Park"
-};
-
-assertEqual(findKeyByValue(parksInVan, "Stanley Park"), "WestEnd");
-assertEqual(findKeyByValue(parksInVan, "Emery Barnes Park"), "Yaletown");
-assertEqual(findKeyByValue(parksInVan, "Pacific Spirit Park"), "UBC");
